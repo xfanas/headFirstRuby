8ede1024e5d8bf7d1d6da74f8c6e1210e388b11b
Fan Chuanqi <fv05@live.com>
2017-06-04 12:57:47 +0800

    add Cat and Bird class

--- firstClass.rb
+++ firstClass.rb
@@ -1,3 +1,12 @@
+class Bird
+  def talk
+    puts "Chirp! Chirp!"
+  end
+  def move(destination)
+    puts "Flying to the #{destination}."
+  end
+end
+
 class Dog
   def talk
     puts "Bark!"
@@ -8,7 +17,20 @@ class Dog
   end
 end
 
-fido = Dog.new
-rex = Dog.new
-fido.talk
-rex.move("food bowl")
+class Cat
+  def talk
+    puts "Meow!"
+  end
+  def move(destination)
+    puts "Running to the #{destination}."
+  end
+end
+
+bird = Bird.new
+dog = Dog.new
+cat = Cat.new
+
+bird.move("tree")
+dog.talk
+bird.talk
+cat.move("house")
