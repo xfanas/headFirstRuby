68ff67799a7b77276de70d0c9c53b1776eebd092
root <root@localhost.localdomain>
2017-06-05 16:47:39 +0800

    last version of chapter03

---
+++ animalsbyinheritance.rb
@@ -0,0 +1,86 @@
+class Animal
+ 
+  attr_reader :name, :age
+
+  def name=(value)
+    if value == ""
+      raise "Name can't be blank!"
+    end
+    @name = value
+  end
+
+  def age=(value)
+    if value < 0
+      raise "An age of #{value} isn't valid!"
+    end
+    @age = value
+  end
+
+  def talk
+    puts "#{@name} says Bark!"
+  end
+
+  def move(destination)
+    puts "#{@name} runs to the #{destination}."
+  end
+
+  def report_age
+    puts "#{@name} is #{@age} year old."
+  end
+
+end
+
+class Dog < Animal
+  def to_s
+    "#{@name} the dog, age #{age}"
+  end
+end
+
+class Bird < Animal
+  
+  def talk
+    puts "#{@name} says Chirp! Chirp!"
+  end
+
+  def move(destination)
+    puts "#{@name} flies to the #{destination}."
+  end
+
+end
+
+class Cat < Animal
+
+  def talk
+    puts "#{name} says Meow!"
+  end
+
+end
+
+class Armadillo < Animal
+
+  def move(destination)
+    puts "#{@name} unrolled!"
+    super
+  end
+
+end
+
+
+whiskers = Cat.new
+whiskers.name = "Whiskers"
+polly = Bird.new
+polly.name = "Polly"
+whiskers.talk
+polly.talk
+
+puts "to be output"
+
+lucy = Dog.new
+lucy.name = "Lucy"
+lucy.age = 4
+
+rex = Dog.new
+rex.name = "Rex"
+rex.age = 2
+
+puts lucy, rex
